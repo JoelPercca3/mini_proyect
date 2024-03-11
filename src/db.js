3730d87eca35d1628b7e225b23382e2bb0339b6e
Joel Percca <joelpercca3@gmail.com>
2024-03-11 03:51:19 -0500

    first commit

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 import mysql2 from 'mysql2'
-import { DB_DATABASE } from './config.js'
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from './config.js'
+
 export const pool = mysql2.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'usuarios.csv'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
 })
